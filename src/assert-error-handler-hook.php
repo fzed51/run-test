013c83eef98143963c744e3dff1163ae0e245a1e
Fabien Sanchez <fzed51+git@gmail.com>
2019-07-16 10:18:31 +0200

    fix | correction du test de leve d'exception

--- src/assert-error-handler-hook.php
+++ src/assert-error-handler-hook.php
@@ -12,49 +12,51 @@ use RuntimeException;
 use Throwable;
 use function set_error_handler;
 
-///** @noinspection TypeUnsafeComparisonInspection */
-//if (getenv('RUNTEST') !== false && (
-//        strtolower(getenv('RUNTEST')) === 'on'
-//        || strtolower(getenv('RUNTEST')) === 'yes'
-//        || getenv('RUNTEST') === true
-//        || getenv('RUNTEST') == 1
-//    )) {
-
-    /**
-     * Class Exception
-     * @package Assert
-     */
+/**
+ * Class Exception
+ * @package Assert
+ */
 class Exception extends RuntimeException
-    {
+{
+    /**
+     * Assert\Exception constructor.
+     * @param string $message
+     * @param int $code
+     * @param \Throwable|null $previous
+     */
     public function __construct(string $message = '', int $code = 0, Throwable $previous = null)
-        {
-            parent::__construct($message, $code, $previous);
-            if ($previous === null) {
-                $backTrace = debug_backtrace();
-                $lastBackTrace = $backTrace[1];
-                if (isset($lastBackTrace['file'])) {
-                    $this->setFilePosition($lastBackTrace['file'], $lastBackTrace['line']);
-                } elseif (isset($lastBackTrace['function']) && $lastBackTrace['function'] === 'Assert\{closure}') {
-                    $this->setFilePosition($lastBackTrace['args'][2], $lastBackTrace['args'][3]);
-                }
-            } else {
-                $this->setFilePosition($previous->getFile(), $previous->getLine());
+    {
+        parent::__construct($message, $code, $previous);
+        if ($previous === null) {
+            $backTrace = debug_backtrace();
+            $lastBackTrace = $backTrace[1];
+            if (isset($lastBackTrace['file'])) {
+                $this->setFilePosition($lastBackTrace['file'], $lastBackTrace['line']);
+            } elseif (isset($lastBackTrace['function']) && $lastBackTrace['function'] === 'Assert\{closure}') {
+                $this->setFilePosition($lastBackTrace['args'][2], $lastBackTrace['args'][3]);
             }
-        }
-
-        public function setFilePosition(string $file, int $line): Exception
-        {
-            $this->file = $file;
-            $this->line = $line;
-            return $this;
+        } else {
+            $this->setFilePosition($previous->getFile(), $previous->getLine());
         }
     }
 
-set_error_handler(function ($severity, $message, $file, $line) {
+    public function setFilePosition(string $file, int $line): Exception
+    {
+        $this->file = $file;
+        $this->line = $line;
+        return $this;
+    }
+}
+
+/**
+ * Modification du error handler de PHP
+ */
+set_error_handler(static function ($severity, $message, $file, $line) {
     throw (new Exception($message, $severity))->setFilePosition($file, $line);
 });
 
 /**
+ * Fonction de test basique
  * @param bool $boolTest
  * @param string $message
  */
@@ -66,6 +68,7 @@ function boolTest(bool $boolTest, string $message = 'doit etre vrai'): void
 }
 
 /**
+ * Fonction interne de validation de schema
  * @param $structure
  * @param $data
  * @param string $location
@@ -146,6 +149,20 @@ function ValidateDataSchema ($structure, $data, $location = '$'): array
 }
 
 /**
+ * Fonction interne de détection de tableau associatif
+ * @param array $arr
+ * @return bool
+ */
+function isAssoc(array $arr): bool
+{
+    if ([] === $arr) {
+        return false;
+    }
+    return array_keys($arr) !== range(0, count($arr) - 1);
+}
+
+/**
+ * Fonction de test de schema de JSON
  * @param $schema - description de la structure du JSON
  * @param string $jsonString - chaine de caractère du JSON
  * @param string $message
@@ -164,49 +181,39 @@ function schemaJsonTest($schema, string $jsonString, string $message = 'structur
 
 
 /**
-     * @param callable $fn
-     * @param \Exception $exceptionAttendu
-     * @param string $message
-     */
-    function throwTest(callable $fn, \Exception $exceptionAttendu, string $message = 'doit lever une exception'): void
-    {
-        try {
-            $fn();
-            throw new Exception('[NO EXCEPTION] ' . $message);
-        } catch (Throwable $t) {
-            $classExceptionAttendu = get_class($exceptionAttendu);
-            $classException = get_class($t);
-            $parentsClassException = class_parents($t);
-            if (!($classExceptionAttendu === $classException) || in_array($classExceptionAttendu, $parentsClassException, true)) {
-                throw new Exception('[BAD EXCEPTION] ' . $message, $t->getCode(), $t);
-            }
-        }
-    }
-
-
-function isAssoc(array $arr)
+ * Fonction de test avec exception
+ * @param callable $fn
+ * @param \Exception $exceptionAttendu
+ * @param string $message
+ */
+function throwTest(callable $fn, \Exception $exceptionAttendu, string $message = 'doit lever une exception'): void
 {
-    if ([] === $arr) {
-        return false;
+    try {
+        $fn();
+    } catch (Throwable $t) {
+        $classExceptionAttendu = get_class($exceptionAttendu);
+        if (!is_a($t, $classExceptionAttendu)) {
+            throw new Exception('[BAD EXCEPTION] ' . $message, $t->getCode(), $t);
+        }
+        return;
     }
-    return array_keys($arr) !== range(0, count($arr) - 1);
+    throw new Exception('[NO EXCEPTION] ' . $message);
 }
 
-    /**
-     * @param callable $fn
-     * @param string $message
-     */
-    function noThrowTest(callable $fn, string $message = "ne doit pas lever d'exception"): void
-    {
-        try {
-            $fn();
-        } catch (Throwable $t) {
-            throw new Exception(
-                $message .
-                ' [' . get_class($t) . '(' . $t->getMessage() . ')]' .
-                '-->(' . $t->getFile() . ':' . $t->getLine() . ')'
-            );
-        }
+/**
+ * Fonction de test sans exception
+ * @param callable $fn
+ * @param string $message
+ */
+function noThrowTest(callable $fn, string $message = "ne doit pas lever d'exception"): void
+{
+    try {
+        $fn();
+    } catch (Throwable $t) {
+        throw new Exception(
+            $message .
+            ' [' . get_class($t) . '(' . $t->getMessage() . ')]' .
+            '-->(' . $t->getFile() . ':' . $t->getLine() . ')'
+        );
     }
-
-//}
+}
--- test/spec-assert/test_assert_fn.php
+++ test/spec-assert/test_assert_fn.php
@@ -14,46 +14,61 @@ echo PHP_EOL;
 Assert\boolTest(true);
 try {
     Assert\boolTest(false);
-    throw \Exception("Doit retourner une \Assert\Exception");
+    throw Exception("Doit retourner une \Assert\Exception");
 } catch (\Assert\Exception $e) {
 }
 
 echo 'Test de Assert\throwTest';
 echo PHP_EOL;
-Assert\throwTest(function () {
-    throw new \Exception("test");
-}, new \Exception());
+Assert\throwTest(static function () {
+    throw new Exception("test");
+}, new Exception());
 
-class A extends \Exception
+class A extends Exception
 {
 }
 
-class B extends \Exception
+class B extends Exception
+{
+}
+
+class C extends B
 {
 }
 
 try {
-    Assert\throwTest(function () {
-    }, new \Exception());
-    throw new \Exception("Doit retourner une \Assert\Exception");
+    Assert\throwTest(static function () {
+    }, new Exception());
+    throw new Exception("Doit retourner une \Assert\Exception");
 } catch (\Assert\Exception $e) {
 }
+
 try {
-    Assert\throwTest(function () {
-        throw new \A();
-    }, new \B());
-    throw new \Exception("Doit retourner une \Assert\Exception");
+    Assert\throwTest(static function () {
+        throw new A();
+    }, new B());
+    throw new Exception("Doit retourner une \Assert\Exception");
 } catch (\Assert\Exception $e) {
 }
+
+Assert\throwTest(static function () {
+    throw new A();
+}, new A());
+
+Assert\throwTest(static function () {
+    throw new C();
+}, new B());
 
 echo 'Test de Assert\noThrowTest';
 echo PHP_EOL;
-Assert\noThrowTest(function () {
+
+Assert\noThrowTest(static function () {
 });
+
 try {
-    Assert\noThrowTest(function () {
-        throw new \Exception();
+    Assert\noThrowTest(static function () {
+        throw new Exception();
     });
-    throw new \Exception("Doit retourner une \Assert\Exception");
+    throw new Exception("Doit retourner une \Assert\Exception");
 } catch (\Assert\Exception $e) {
 }
